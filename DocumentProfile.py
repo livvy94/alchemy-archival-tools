08551cb284bce1057d4551b40f162124852cc693
livvy94 <haxxor199x@gmail.com>
2023-07-14 19:07:22 -0500

    Remove weird __str__ redefinition

--- DocumentProfile.py
+++ DocumentProfile.py
@@ -12,17 +12,5 @@ class DocumentProfile:
         self.totalassets = totalassets
         self.zipcode = zipcode
 
-    def __str__(self):
-        result = f"====  Document Profile for {self.ein}  ====\n"
-        result += f"Taxpayer Name: {self.taxpayername}\n"
-        result += f"State: {self.state}\n"
-        result += f"ZIP Code: {self.zipcode}\n"
-        result += f"Return Type: {self.returntype}\n"
-        result += f"Sub Date: {self.subdate}\n"
-        result += f"Tax Year: {self.taxyear}\n"
-        result += f"Sub Code: {self.subcode}\n"
-        result += f"Total Assets: {self.totalassets}\n"
-        return result
-
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
