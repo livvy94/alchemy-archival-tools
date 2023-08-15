24f8310d04305a4b1e20630898372db10f199d2f
livvy94 <haxxor199x@gmail.com>
2023-08-15 10:05:28 -0500

    Finish TIFF extraction, just needs to save 'em
    blah

--- extract_tiffs.py
+++ extract_tiffs.py
@@ -1,15 +1,75 @@
-from secrets import list_of_files
+from secrets import list_of_TIFF_blobs
 from pathlib import Path
 
 def main():
-    print("Ta-da!")
+    for filename in list_of_TIFF_blobs:
+        current_title = get_cdrom_name(filename) #This should be identical to the folder name extract_metadata.py gets
+
+        with open(filename, 'rb') as f:
+            data = f.read() # This "data" variable is where the entire file's contents is going to be stored
+
+        all_TIFFs = extract(data)
+        # TODO: Save each TIFF file!
+        # Dump everything in a folder called the name of the CD (stored in current_title above!)
+
+def extract(data):
+    separated_TIFF_files = []
+    TIFF_offsets = get_list_of_offsets(data, '49492A00')
+
+    for index, offset in enumerate(TIFF_offsets):
+        # The next line figures out where the end of the current file is.
+        # If this is in the middle of the list, it's just the next offset.
+        # If we're at the end of the list, though, we can't do that - index + 1 doesn't exist.
+        # To make it work, we can just use the length of the file itself as the last offset.
+        second_offset = TIFF_offsets[index + 1] if index + 1 < len(TIFF_offsets) else len(data)
+        print(f"{offset} to {second_offset}")  # debug, delete this line
+        separated_TIFF_files.append(data[offset:second_offset])  # Slice out the individual file
+
+    return separated_TIFF_files
+
 ########################################################################
+
+# Helper methods!
+def get_cdrom_name(filename):
+    return Path(filename).resolve().parents[2].name # This takes the whole filepath and returns just the name of the folder.
+
+def convert_to_bytes(input):
+    return bytes.fromhex(input) # Converts a string that contains a hex number into that number. Simple but this looks cleaner.
+
+def get_list_of_offsets(data, header_hex):
+    start_code = convert_to_bytes(header_hex) # Convert it to a byte array so we can use data.find with it
+
+    offset_list = [] # Find the location of each time these bytes show up in the given file
+    pos = data.find(start_code) # Find the first instance of the start code
+
+    while pos != -1: # Loop until it can't find any more
+      offset_list.append(pos)
+      pos = data.find(start_code, pos + 1) # Look for another one!
+
+    return offset_list
+
+
 
 # Now that everything's defined, run the dang thing!
 if __name__ == "__main__":
     main()
 
 # NOTES
+# Here's an example of someone doing this sort of thing in a different
+# programming language:
+# https://superuser.com/questions/405449/split-concatenated-tiff-file
+# And here's a useful page linked in one of the answers here:
+# https://www.fileformat.info/format/tiff/corion.htm
+
+# The tiff header is [49 49 2A 00]
+# In ASCII, [49 49] is II. This indicates that the TIFF file is Intel byte-order.
+# Other options include MM, which indicates Motorola byte-order. For the purposes
+# of this project, I'm assuming all of them start with II.
+# The [2A 00] is hex for the number 42, chosen for its deep philosophical value.
+# Next in the header, the offset of the first image directory. Since this is
+# different for each file, I'm not putting anything in for it when searching
+# through the file.
+
 # It might be possible to use PFAL000S.TOB to get the name
 # of each record, and each filename as it appears when you
 # open it in Alchemy. However, the Fall semester is about
@@ -23,3 +83,5 @@ if __name__ == "__main__":
 # Dump from [offset] to [next offset]
 # If it's the last file, dump from [offset] to the end of
 # the file
+
+# TODO: Put copied and pasted helper methods into a common place?
