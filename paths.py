9cb8cca3e565781af9ff10e58705a64be2be6d93
livvy94 <haxxor199x@gmail.com>
2023-08-27 12:40:14 -0500

    Separate path to make it easier to read

--- paths.py
+++ paths.py
@@ -1,10 +1,9 @@
+base_path = r"C:\Users\vince\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005"
+
 list_of_metadata_files = [
-    r"C:\Users\vince\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005\Mss005_0001\1998 Jul - Dec; Private Foundation Returns for ALABAMA; pfal000s001\PFAL000S\DATA000\00001.PRO",
-    #r"C:\Users\vince\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005\Mss005_0002\1998 Jul - Dec; PFS - Alabama; Vol pfal000s001\PFAL000S\DATA000\00001.PRO",
-    #r"C:\Users\vince\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005\Mss005_0003\1998 Jul - Dec; PFS Arkansas; Vol pfar000u001\PFAR000V\DATA000\00001.PRO",
-    #r"C:\Users\vince\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005\Mss005_0006\Jul thru Dec 1998; Private Foundation Returns for ILLINOIS; pfil007p001\PFIL007P\DATA000\00001.PRO",
+    base_path + r"\Mss005_0001\1998 Jul - Dec; Private Foundation Returns for ALABAMA; pfal000s001\PFAL000S\DATA000\00001.PRO",
 ]
 
 list_of_TIFF_blobs = [
-    r"C:\Users\vince\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005\Mss005_0001\1998 Jul - Dec; Private Foundation Returns for ALABAMA; pfal000s001\PFAL000S\DATA000\00001.DAT",
+    base_path + r"\Mss005_0001\1998 Jul - Dec; Private Foundation Returns for ALABAMA; pfal000s001\PFAL000S\DATA000\00001.DAT",
 ]
